1352100c7a6e0c87221be8b3c37531efab655b81
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2025-10-08 21:27:07 +0300

    Create AiToolController.java

---
+++ src/main/java/com/ozalp/AI/Generator/Backend/controller/AiToolController.java
@@ -0,0 +1,28 @@
+package com.ozalp.AI.Generator.Backend.controller;
+
+import com.ozalp.AI.Generator.Backend.business.abstracts.AiToolService;
+import com.ozalp.AI.Generator.Backend.business.dtos.requests.concretes.CreateAiToolRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/aiTool/")
+public class AiToolController {
+
+    private final AiToolService aiToolService;
+
+    @PostMapping(name = "create")
+    public ResponseEntity<?> create(@RequestBody CreateAiToolRequest request) {
+        return ResponseEntity.ok(aiToolService.create(request));
+    }
+
+    @PostMapping(name = "delete")
+    public ResponseEntity<?> delete(@RequestParam UUID uuid) {
+        return ResponseEntity.ok(aiToolService.delete(uuid));
+    }
+
+}
