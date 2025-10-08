6ed02121292d4a6f687120c0ae4365ef2aa99269
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2025-10-08 16:31:58 +0300

    Initial commit

---
+++ src/main/java/com/ozalp/AI/Generator/Backend/AiGeneratorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ozalp.AI.Generator.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiGeneratorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiGeneratorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ozalp/AI/Generator/Backend/AiGeneratorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozalp.AI.Generator.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiGeneratorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
